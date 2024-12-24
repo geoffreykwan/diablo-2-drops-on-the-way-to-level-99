28020d1217afdf4b8c52d172736d5294c766d3b1
Geoffrey Kwan <geoffreykwan@gmail.com>
2024-12-23 22:06:38 -0500

    feat(Drops Page): Move data folder into src and show items on drops page

--- src/views/Drops.vue
+++ src/views/Drops.vue
@@ -1,3 +1,27 @@
 <template>
-  <div>Drops</div>
+  <h1>Drops</h1>
+  <div v-for="item in items" :key="item.name">
+    <div>{{ item.name }}</div>
+  </div>
 </template>
+
+<script>
+import uniques from '../data/uniques.ts'
+import runes from '../data/runes.ts'
+import setBonuses from '../data/set-bonuses.ts'
+console.log(uniques)
+console.log(runes)
+console.log(setBonuses)
+const items = [...uniques, ...runes]
+export default {
+  name: 'Drops',
+  data() {
+    return {
+      items: items,
+      uniques: uniques,
+      runes: runes,
+      setBonuses: setBonuses
+    }
+  }
+}
+</script>
