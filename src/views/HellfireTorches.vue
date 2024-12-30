427927794f8a5b2fde3b9b1d7457c26e4da0b3c2
Geoffrey Kwan <geoffreykwan@gmail.com>
2024-12-29 21:46:48 -0500

    feat(Torches Page): Display torches

--- src/views/HellfireTorches.vue
+++ src/views/HellfireTorches.vue
@@ -1,3 +1,28 @@
 <template>
-  <div>Hellfire Torches</div>
+  <h1>Hellfire Torches</h1>
+  <div class="torch-row">
+    <div>+ 3 Class Skills</div>
+    <div>+ All Attributes</div>
+    <div>+ All Resistances</div>
+  </div>
+  <div class="torch-rows">
+    <div v-for="hellfireTorch in hellfireTorches" :key="hellfireTorch" class="torch-row">
+      <div>{{ hellfireTorch.class }}</div>
+      <div>{{ hellfireTorch.attributes }}</div>
+      <div>{{ hellfireTorch.resistances }}</div>
+    </div>
+  </div>
 </template>
+
+<script>
+import '../styles/hellfire-torches.css'
+import hellfireTorches from '../data/hellfire-torches.json'
+export default {
+  name: 'HellfireTorches',
+  data() {
+    return {
+      hellfireTorches: hellfireTorches,
+    }
+  },
+}
+</script>
