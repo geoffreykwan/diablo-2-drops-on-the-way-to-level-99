06b670d65b9f42c474a8c8eabb120233ae7e4c9d
Geoffrey Kwan <geoffreykwan@gmail.com>
2024-12-29 22:24:30 -0500

    feat(Plus Skill Grand Charms Page): Display plus skill grand charms

--- src/views/PlusSkillGrandCharms.vue
+++ src/views/PlusSkillGrandCharms.vue
@@ -1,3 +1,24 @@
 <template>
-  <div>+Skill Grand Charms</div>
+  <h1>+Skill Grand Charms</h1>
+  <div v-for="plusSkillGrandCharm in plusSkillGrandCharms" :key="plusSkillGrandCharm.class"
+    class="plus-skill-grand-charm-row">
+    <div class="class-name">{{ plusSkillGrandCharm.class }}</div>
+    <div v-for="skillTab in plusSkillGrandCharm.skillTabs" :key="skillTab.name" class="skill-tab">
+      <div>{{ skillTab.name }}</div>
+      <div>{{ skillTab.count }}</div>
+    </div>
+  </div>
 </template>
+
+<script>
+import '../styles/plus-skill-grand-charms.css'
+import plusSkillGrandCharms from '../data/plus-skill-grand-charms.json'
+export default {
+  name: 'PlusSkillGrandCharms',
+  data() {
+    return {
+      plusSkillGrandCharms: plusSkillGrandCharms,
+    }
+  },
+}
+</script>
