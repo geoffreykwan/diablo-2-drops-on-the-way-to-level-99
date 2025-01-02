e976d0aa358c64242fdd2c3e2a4349b9c79cda02
Geoffrey Kwan <geoffreykwan@gmail.com>
2025-01-01 21:06:01 -0500

    feat(Hellfire Torches Page): Add sort by class, attributes, resistances

--- src/views/HellfireTorches.vue
+++ src/views/HellfireTorches.vue
@@ -1,5 +1,9 @@
 <template>
   <h1>Hellfire Torches</h1>
+  Sort By
+  <button @click="sortByClass">+ 3 Class Skills</button>
+  <button @click="sortByAttributes">+ All Attributes</button>
+  <button @click="sortByResistances">+ All Resistances</button>
   <div class="torch-row">
     <div>+ 3 Class Skills</div>
     <div>+ All Attributes</div>
@@ -24,5 +28,49 @@ export default {
       hellfireTorches: hellfireTorches,
     }
   },
+  mounted() {
+    this.sortByClass()
+  },
+  methods: {
+    sortByClass() {
+      this.hellfireTorches.sort((a, b) => {
+        if (a.class === b.class) {
+          if (a.attributes === b.attributes) {
+            return b.resistances - a.resistances
+          } else {
+            return b.attributes - a.attributes
+          }
+        } else {
+          return a.class.localeCompare(b.class)
+        }
+      })
+    },
+    sortByAttributes() {
+      this.hellfireTorches.sort((a, b) => {
+        if (a.attributes === b.attributes) {
+          if (a.resistances === b.resistances) {
+            return a.class.localeCompare(b.class)
+          } else {
+            return b.resistances - a.resistances
+          }
+        } else {
+          return b.attributes - a.attributes
+        }
+      })
+    },
+    sortByResistances() {
+      this.hellfireTorches.sort((a, b) => {
+        if (a.resistances === b.resistances) {
+          if (a.attributes === b.attributes) {
+            return a.class.localeCompare(b.class)
+          } else {
+            return b.attributes - a.attributes
+          }
+        } else {
+          return b.resistances - a.resistances
+        }
+      })
+    },
+  }
 }
 </script>
