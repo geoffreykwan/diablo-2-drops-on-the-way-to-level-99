86776007456194aceb9d95d369fb339680c87666
Geoffrey Kwan <geoffreykwan@gmail.com>
2025-01-08 15:49:56 -0500

    feat(Character Page): Add character screenshots

--- src/views/Character.vue
+++ src/views/Character.vue
@@ -1,3 +1,8 @@
 <template>
-  <div>Character</div>
+  <h1>Character</h1>
+  <div v-for="index in 41">
+    <img :src="'../src/assets/character/character-' + index.toString().padStart(2, '0') + '.jpg'" />
+    <br />
+    <br />
+  </div>
 </template>
