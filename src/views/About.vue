ee5ceb39e36a75473bb396c5e51bfd91e39fe162
Geoffrey Kwan <geoffreykwan@gmail.com>
2025-01-12 22:20:10 -0500

    feat(About Page): Change styling

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,10 +1,12 @@
 <template>
   <h1>About</h1>
-  <div v-for="info in about" :key="info" class="align-left">
+  <div v-for="info in about" :key="info" class="align-left2">
     <div class="question">{{ info.question }}</div>
-    <div v-if="Array.isArray(info.answer)" v-for="answerLine in info.answer" :key="answerLine">{{
-      answerLine }}</div>
-    <div v-else>{{ info.answer }}</div>
+    <div v-if="Array.isArray(info.answer)" v-for="answerLine in info.answer" :key="answerLine"
+      class="answer">
+      {{ answerLine }}
+    </div>
+    <div v-else class="answer">{{ info.answer }}</div>
     <br />
   </div>
 </template>
