774fc7b8f5205af36b3a8f646b71b454d2dbe5e7
Geoffrey Kwan <geoffreykwan@gmail.com>
2025-01-12 20:51:30 -0500

    feat(About Page): Add information to About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,3 +1,23 @@
 <template>
-  <div>About</div>
+  <h1>About</h1>
+  <div v-for="info in about" :key="info" class="align-left">
+    <div class="question">{{ info.question }}</div>
+    <div v-if="Array.isArray(info.answer)" v-for="answerLine in info.answer" :key="answerLine">{{
+      answerLine }}</div>
+    <div v-else>{{ info.answer }}</div>
+    <br />
+  </div>
 </template>
+
+<script>
+import '../styles/about.css'
+import about from '../data/about.json'
+export default {
+  name: 'About',
+  data() {
+    return {
+      about: about
+    }
+  }
+}
+</script>
