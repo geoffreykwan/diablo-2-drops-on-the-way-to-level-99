c5866a37573575a16159fa33707cecb9b64cf102
Geoffrey Kwan <geoffreykwan@gmail.com>
2025-01-12 22:02:38 -0500

    feat(Drops Page): Show item names in appropriate color

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -4,7 +4,13 @@ defineProps({ item: Object, showStats: Boolean })
 <template>
   <div v-if="item != null">
     <li v-if="showStats" class="stat-line"><img :src="'../src/assets/items/' + item.image" /></li>
-    <li class="stat-line">{{ item.name }}</li>
+    <li class="stat-line" :class="{
+      'set-item': item.setName != undefined,
+      'rune': item.type == 'Rune',
+      'unique-item': item.setName == undefined && item.type != 'Rune'
+    }">
+      {{ item.name }}
+    </li>
     <li v-if="showStats" class="stat-line">{{ item.type }}</li>
   </div>
 </template>
