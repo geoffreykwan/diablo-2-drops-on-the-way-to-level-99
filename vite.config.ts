fb77cce863b7f6cbb248704e9389b2e3a64d49ed
Geoffrey Kwan <geoffreykwan@gmail.com>
2025-01-19 21:20:29 -0500

    build(Site): Add repo name to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'url';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'diablo-2-drops-on-the-way-to-level-99',
   plugins: [vue()],
   resolve: {
     alias: {
